e7c8d196a585b6a9ca89dbcc4624bc7c46d60698
auc7us <keshavsharan97@gmail.com>
2025-04-27 17:11:18 -0500

    moved client folder inside server

--- server/client/app.js
+++ server/client/app.js
@@ -3,8 +3,8 @@ const remote_video = document.getElementById('remote_video');
 const error_display = document.getElementById('error_display');
 
 async function init() {
-  console.log('Connect button clicked. Starting handhshake');
-  const url = 'https://localhost:4433/connect';
+  console.log('Connect button clicked');
+  const url = 'https://localhost:4433/wt';
 
   let transport;
   try {
@@ -14,10 +14,6 @@ async function init() {
     console.log('wt session is ready');
     error_display.textContent = 'Connected';
 
-    transport.closed
-      .then(() => console.log('session closed normally'))
-      .catch((err) => console.warn('session closed unexpectedly', err));
-
     const { readable, writable } = await transport.createBidirectionalStream();  
 
     const writer = writable.getWriter();
@@ -25,7 +21,7 @@ async function init() {
     const encoder = new TextEncoder();
     const decoder = new TextDecoder();
 
-    await writer.write(encoder.encode('Hello from client!'));
+    await writer.write(encoder.encode('hello world!'));
 
     (async () => {
       while (true) {
